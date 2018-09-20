6955f5bc676dcc85a6b900df2316a916fde0bac9
Daniel Lumoindong <daniel.lumoindong@gmail.com>
2018-09-20 11:18:46 +0700

    2nd Initial commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2ldpf1l8*%8(yj*-#t7%g)mdne1k5s9_izz2wo2#*pqaudkj7t'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
