fba3e8162f6e87f1fa3f74a789f44ceb44aa8904
Daniel Lumoindong <daniel.lumoindong@gmail.com>
2018-09-23 07:07:17 +0700

    changing of Setting.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from decouple import config
+from decouple import config, Csv
+import dj_database_url
+
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOST', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
